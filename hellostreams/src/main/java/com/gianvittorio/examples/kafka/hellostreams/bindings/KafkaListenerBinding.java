48e7a3a05d3433decf7ba9cceff1fe955d336863
gianvittorio <gianvittorio.castellano@gmail.com>
2021-07-04 13:42:32 -0300

    done with simple consumer

---
+++ hellostreams/src/main/java/com/gianvittorio/examples/kafka/hellostreams/bindings/KafkaListenerBinding.java
@@ -0,0 +1,10 @@
+package com.gianvittorio.examples.kafka.hellostreams.bindings;
+
+import org.apache.kafka.streams.kstream.KStream;
+import org.springframework.cloud.stream.annotation.Input;
+
+public interface KafkaListenerBinding {
+
+    @Input("input-channel-1")
+    KStream<String, String> inputStream();
+}
---
+++ hellostreams/src/main/java/com/gianvittorio/examples/kafka/hellostreams/service/KafkaListenerService.java
@@ -0,0 +1,19 @@
+package com.gianvittorio.examples.kafka.hellostreams.service;
+
+import com.gianvittorio.examples.kafka.hellostreams.bindings.KafkaListenerBinding;
+import lombok.extern.log4j.Log4j2;
+import org.apache.kafka.streams.kstream.KStream;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Log4j2
+@EnableBinding(KafkaListenerBinding.class)
+public class KafkaListenerService {
+
+    @StreamListener("input-channel-1")
+    public void process(KStream<String, String> input) {
+        input.foreach((k, v) -> log.info("Key: {}, Value: {}", k, v));
+    }
+}
